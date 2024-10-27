2ffc886a59d98f250af251affc11f7aaac3a455f
Ahmed-Rakha <rakhaahmed1@gmail.com>
2024-10-27 23:09:50 +0300

    fix routes

--- src/routes/_NavLayout.admin/dashboard.jsx
+++ src/routes/_NavLayout.admin/dashboard.jsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import DashboardHeader from "../../components/DashboardHeader";
-import Sidebar from "../../components/Sidebar";
+import Sidebar from "../../components/SideBar";
 import { CssBaseline } from "@mui/material";
 import ChartsOverviewDemo from "../../components/Overview";
 export const Route = createFileRoute("/_NavLayout/admin/dashboard")({
