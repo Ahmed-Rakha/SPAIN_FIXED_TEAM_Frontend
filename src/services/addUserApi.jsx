1ed0e5df9a7a752342ed2455b154a218b5b0d1c4
Ahmed-Rakha <rakhaahmed1@gmail.com>
2025-03-16 23:29:06 +0200

    [change]: change VITE_API_URL value to the new one: fixedspainteambackend-production-7999.up.railway.app

--- src/services/addUserApi.jsx
+++ src/services/addUserApi.jsx
@@ -2,10 +2,10 @@ const API_URL = import.meta.env.VITE_API_URL;
 export default async function addUserApi(data) {
   try {
     const response = await fetch(`${API_URL}/api/auth/register`, {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify(data),
       headers: {
-        'Content-type': 'application/json; charset=UTF-8',
+        "Content-type": "application/json; charset=UTF-8",
       },
     });
 
@@ -15,16 +15,16 @@ export default async function addUserApi(data) {
     }
 
     if (!response.ok) {
-      throw new Error('Failed to add user. Please try again.');
+      throw new Error("Failed to add user. Please try again.");
     }
     let { message } = await response.json();
     if (!message) {
-      throw new Error('Failed to add user. Please try again.');
+      throw new Error("Failed to add user. Please try again.");
     }
-    message = 'Usuario agregado exitosamente';
+    message = "Usuario agregado exitosamente";
     return message;
   } catch (error) {
-    console.error('Error adding user:', error);
+    console.error("Error adding user:", error);
     throw error;
   }
 }
